229dadd52e8bab428133ea69edb5e74acecd8f21
light lu <light.lu@advantech.com.tw>
2021-05-31 10:00:11 +0800

    dotnet new console; dotnet restore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RefrigeratedTruck._0531
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
